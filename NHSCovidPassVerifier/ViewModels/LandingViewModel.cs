d8ab0a7baa11f98a0a2716f937cde3160fe7f9bd
Dan <git@daniel-ross.com>
2021-07-25 16:32:53 +0100

    ViewModel and localisation to support Reference Data functionality

--- NHSCovidPassVerifier/ViewModels/LandingViewModel.cs
+++ NHSCovidPassVerifier/ViewModels/LandingViewModel.cs
@@ -13,22 +13,37 @@ namespace NHSCovidPassVerifier.ViewModels
     public class LandingViewModel : BaseViewModel
     {
         private readonly ISecureStorageService<TermsAndConditionsAgreed> _secureStorageService;
+        private readonly IJwkService _jwks;
+
         public string LandingsPageImageSource { get; }
         public string OpenScannerText { get; set; }
 
+        public string ReferenceDataStatusText { get; set; }
+        public bool ReferenceDataUpdateRequired { get; set; }
+
         public ICommand NextPageCommand => new AsyncCommand(async () => await ExecuteOnceAsync(NextPage));
 
-        public LandingViewModel(ISecureStorageService<TermsAndConditionsAgreed> secureStorageService)
+        public ICommand UpdateReferenceDataCommand => new AsyncCommand(
+            async () => await ExecuteOnceAsync(UpdateReferenceData),
+            () => ReferenceDataUpdateRequired
+        );
+
+        public LandingViewModel(ISecureStorageService<TermsAndConditionsAgreed> secureStorageService, IJwkService jwks)
         {
             _secureStorageService = secureStorageService;
+            _jwks = jwks;
             
             LandingsPageImageSource = "nhs_logo_white.png";
+            
             InitText();
+            RefreshReferenceDataStatusText();
+        
         }
 
         private void InitText()
         {
             OpenScannerText = "LANDING_PAGE_QR_CODE".Translate();
+            ReferenceDataStatusText = "DATA_UPDATE_LOADING".Translate();
         }
 
         private async Task NextPage()
@@ -70,5 +85,64 @@ namespace NHSCovidPassVerifier.ViewModels
         {
             await _navigationService.PushPage(new AboutPage(),false,true);
         }
+    
+        /// <summary>
+        /// Refreshes the 'Data Last Updated' value displayed
+        /// </summary>
+        /// <returns></returns>
+        private async Task RefreshReferenceDataStatusText()
+        {
+            ReferenceDataStatusText = "DATA_UPDATE_LOADING".Translate();
+            RaisePropertyChanged(() => ReferenceDataStatusText);
+
+            bool updateRequired = false;
+
+            bool jwkDataPresent = await _jwks.JwkListPresent();
+            
+            if (!jwkDataPresent)
+            {
+                updateRequired = true;
+            } 
+            else
+            {
+                updateRequired = await _jwks.JwkListNeedsUpdating();
+            }
+
+            // Push into status prop
+            if (updateRequired)
+            {
+                ReferenceDataStatusText = "DATA_UPDATE_REQUIRED".Translate();
+            } else
+            {
+                ReferenceDataStatusText = "DATA_UPDATE_NOT_REQUIRED".Translate();
+            }
+            RaisePropertyChanged(() => ReferenceDataStatusText);
+
+            // Set the property backing CanExecute of the Update command
+            ReferenceDataUpdateRequired = updateRequired;
+            RaisePropertyChanged(() => ReferenceDataUpdateRequired);
+        }
+
+        private async Task UpdateReferenceData()
+        {
+            ReferenceDataStatusText = "DATA_UPDATE_RUNNING".Translate();
+            RaisePropertyChanged(() => ReferenceDataStatusText);
+
+            bool updateSuccess = await _jwks.UpdateJwkList();
+            if (updateSuccess)
+            {
+                ReferenceDataUpdateRequired = false;
+                ReferenceDataStatusText = "DATA_UPDATE_NOT_REQUIRED".Translate();
+            } 
+            else
+            {
+                ReferenceDataUpdateRequired = true;
+                ReferenceDataStatusText = "DATA_UPDATE_REQUIRED".Translate();
+            }
+
+            RaisePropertyChanged(() => ReferenceDataStatusText);
+            RaisePropertyChanged(() => ReferenceDataUpdateRequired);
+        }
+
     }
 }
